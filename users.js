56d6a9cde7e6b87f510e36600cc1b65b5cb84b9c
SupportCZSK <prostedanek1@gmail.com>
2025-04-15 23:05:26 +0200

    vylepšení verze

---
+++ users.js
@@ -0,0 +1,14 @@
+// users.js
+const users = [
+    { "username": "Danek", "password": "Danek1234567", "rank": "Owner" },
+    { "username": "User1", "password": "userpass", "rank": "Supporter" },
+    { "username": "User2", "password": "userpass", "rank": "Supporter" },
+    { "username": "User3", "password": "userpass", "rank": "Supporter" },
+    { "username": "User4", "password": "userpass", "rank": "Supporter" },
+    { "username": "User5", "password": "userpass", "rank": "Supporter" },
+    { "username": "User6", "password": "userpass", "rank": "Supporter" },
+    { "username": "User7", "password": "userpass", "rank": "Supporter" },
+    { "username": "User8", "password": "userpass", "rank": "Supporter" },
+    { "username": "User9", "password": "userpass", "rank": "Supporter" },
+    { "username": "User10", "password": "userpass", "rank": "Supporter" }
+];
